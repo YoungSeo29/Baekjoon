5b71d0178bfcd5df1c56ea56cbdbf766f2a0137c
youngseo Kim <yseokim0212@gmail.com>
2022-10-28 01:59:38 +0900

    1차원 배열

--- level_4/8958.py
+++ level_4/8958.py
@@ -1,4 +1,17 @@
-n=int(input())
+N=int(input())
 
-for i in range(n):
-    score=input()
+for i in range(N):
+    an=list(input())
+
+    score=0
+    total=0
+
+    for j in range(len(an)):
+        if an[j]=='O':
+            score=score+1
+            total=total+score
+        else :
+            score=0
+
+    print(total)
+        
